dd659c369fa49332e7018d8c751c40aa7862718c
Khalid Madih <madih_khalid@hotmail.com>
2018-02-10 09:29:26 -0700

    adding html routes

---
+++ app/routing/html-routes.js
@@ -0,0 +1,22 @@
+// Make sure to include the path package to route to the correct file 
+var path = require('path');
+
+// Exporting the route function/routes
+
+module.exports = function(app){
+
+	// HTML GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+}

16899c9d639363cbcda67cb2f6e67bfb08e40aae
Khalid Madih <madih_khalid@hotmail.com>
2018-02-10 09:21:24 -0700

    adding data file

---
+++ app/data/friends.js
@@ -0,0 +1,166 @@
+// Data file holding the values
+var friends = [
+  {
+      "name":"John Lennon",
+      "photo":"https://img.discogs.com/J19UWxyuyProuiwhaQSfzeDUchM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-46481-1286626601.jpeg.jpg",
+      "scores":[
+         5,
+         1,
+         4,
+         4,
+         5,
+         1,
+         2,
+         5,
+         4,
+         1
+          ]
+      },
+  {
+  "name":"Michael Jackson",
+  "photo":"https://www.thefamouspeople.com/profiles/images/michael-jackson-3.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"Madonna",
+  "photo":"https://www.usmagazine.com/wp-content/uploads/madonna-1985-533b8da7-4c10-4ba2-8bae-cdc18804c629.jpg",
+  "scores":[
+     9,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"Sting",
+  "photo":"http://img.wennermedia.com/social/rs-sting-607dc4e4-4b5a-4107-93ec-671193153777.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+{
+  "name":"Bob Marley",
+  "photo":"https://www.reggaeville.com/fileadmin/artists/bobmarley.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+{
+  "name":"Justin Bieber",
+  "photo":"https://www.fuse.tv/image/544533319a9551c444000080/316/316/justin-bieber.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+{
+  "name":"Calvin Harris",
+  "photo":"http://inthemix.junkee.com/wp-content/uploads/2018/02/calvin-harris-press-2015-billboard-1548-b-1.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+{
+  "name":"Don Bigg",
+  "photo":"http://www.festivalmawazine.ma/wp-content/uploads/2017/04/Ph-Bigg.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+{
+  "name":"Elissa",
+  "photo":"https://i.pinimg.com/736x/e9/c7/25/e9c72525460db9d94a73d4f4a24238de--arabic-women-arabic-makeup.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+{
+  "name":"Maryam Fariss",
+  "photo":"https://marriedwiki.com/uploads/bio/myriam-fares.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+}
+]
+
+// Exporting the array to other files
+module.exports = friends;
